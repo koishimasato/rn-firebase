47eedf196b757c99c8cc4e208e460af867d9d048
Koishi Masato <koishi@Koishi-no-MacBook-Pro.local>
2017-03-28 11:11:14 +0900

    fix firebaseConfig export style

---
+++ components/firebaseConfig.js
@@ -0,0 +1,15 @@
+const apiKey = "AIzaSyDzfwnuVmjplmX5cjcmjHigbwNYujvg7QI";
+const authDomain = "fireapp-c0373.firebaseapp.com";
+const databaseURL = "https =//fireapp-c0373.firebaseio.com";
+const storageBucket = "fireapp-c0373.appspot.com";
+const messagingSenderId = "459609096379";
+
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: authDomain,
+  databaseURL: databaseURL,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+};
+
+export default firebaseConfig;
